3441adecb2b51b1fdfbc0799814a97617bd50320
ThomasDavidson <thomasdavidson268@gmail.com>
2024-06-17 16:54:43 -0400

    add home support

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use std::process::Command;
 fn get_path() -> Vec<String> {
     let key = "PATH";
     match env::var_os(key) {
-        Some(paths) => {
-            env::split_paths(&paths).map(|p| p.to_str().unwrap().to_string()).collect()
-        }
-        None => Vec::new()
+        Some(paths) => env::split_paths(&paths)
+            .map(|p| p.to_str().unwrap().to_string())
+            .collect(),
+        None => Vec::new(),
     }
 }
 
@@ -27,6 +27,18 @@ fn file_on_path(file: &str) -> Option<String> {
     None
 }
 
+fn get_home() -> Option<String> {
+    let key = "HOME";
+    match env::var_os(key) {
+        Some(path) => {
+            let Ok(str_path) = path.into_string() else {
+                return None;
+            };
+            Some(str_path)
+        }
+        None => None,
+    }
+}
 
 fn main() {
     get_path();
@@ -39,17 +51,16 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let (command, argument) = input.trim().split_once(" ").unwrap_or_else(|| (input.trim(), ""));
-
+        let (command, argument) = input
+            .trim()
+            .split_once(" ")
+            .unwrap_or_else(|| (input.trim(), ""));
 
         match command {
             "exit" => break,
             "echo" => println!("{}", argument),
             "type" => {
-                if ["exit"
-                    , "echo"
-                    , "type"
-                    , "pwd"].contains(&argument) {
+                if ["exit", "echo", "type", "pwd"].contains(&argument) {
                     println!("{} is a shell builtin", argument)
                 } else if let Some(file) = file_on_path(argument) {
                     println!("{} is {}", argument, file)
@@ -68,20 +79,30 @@ fn main() {
                 println!("{}", path.display());
             }
             "cd" => {
-                match set_current_dir(argument) {
+                let path = if argument.contains("$") {
+                    let Some(home) = get_home() else {
+                        println!("cd: {}: No such file or directory", argument);
+                        continue;
+                    };
+                    argument.replace("$", &home)
+                } else {
+                    argument.to_string()
+                };
+
+                match set_current_dir(path) {
                     Ok(_) => continue,
                     Err(_) => println!("cd: {}: No such file or directory", argument),
                 }
             }
             _ => {
                 if let Some(_) = file_on_path(command) {
-                    let output = Command::new("sh")
-                        .arg("-c")
-                        .arg(&input)
-                        .output()
-                        .unwrap();
+                    let output = Command::new("sh").arg("-c").arg(&input).output().unwrap();
 
-                    let fmt_output = output.stdout.into_iter().map(|c| c as char).collect::<String>();
+                    let fmt_output = output
+                        .stdout
+                        .into_iter()
+                        .map(|c| c as char)
+                        .collect::<String>();
                     println!("{}", fmt_output.trim());
                 } else {
                     println!("{}: command not found", command)
